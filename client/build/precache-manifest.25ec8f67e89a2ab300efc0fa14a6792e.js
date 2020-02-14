40a976248988bcd41247db2fc2e62a7a177f7676
Sagar <sagarpatel16@gmail.com>
2020-02-13 21:03:33 -0600

    added build

---
+++ client/build/precache-manifest.25ec8f67e89a2ab300efc0fa14a6792e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fc583abac95a574cb2e80abb435ccba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "76e0c90e6bb8b3afc54c",
+    "url": "./static/js/2.2f045539.chunk.js"
+  },
+  {
+    "revision": "6b1c9aa1f24077e4bfd4b51db2988c14",
+    "url": "./static/js/2.2f045539.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "867bae62981f44b91c54",
+    "url": "./static/js/main.32febf1d.chunk.js"
+  },
+  {
+    "revision": "1265a379fe4e49039387",
+    "url": "./static/js/runtime-main.e2cbee3d.js"
+  }
+]);
