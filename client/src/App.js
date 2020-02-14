ef574ceb048c454b637ea51dec0dd8f81bf06af4
Sagar <sagarpatel16@gmail.com>
2020-02-13 20:58:24 -0600

    please fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NoMatchFoundData from "./pages/NoMatchFound";
 import Saved from "./pages/SavedBooks";
 import NavBar from "./components/navbar";
 
-function App () {
+function App() {
   return (
     <Router>
       <div>
         <NavBar />
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/saved" component={ Saved } />
-          <Route component={ NoMatchFoundData } />
-          
+          <Route exact path="/saved" component={Saved} />
+          <Route path="/" component={Home} />
+          {/* <Route component={ NoMatchFoundData } /> */}
         </Switch>
       </div>
     </Router>
   );
-};
+}
 
 export default App;
